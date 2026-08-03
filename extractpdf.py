4e310e6193a6784497817a7ee55d3c26df81238c
Azure1506-afk <rajasekhar.pappalabooks@gmail.com>
2026-08-03 17:24:40 -0400

    Add initial implementation of extractpdf.py with embedding model loading

---
+++ extractpdf.py
@@ -0,0 +1,21 @@
+import os
+import langchain
+import pytesseract
+from PyPDF2 import PdfReader
+#from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain.schema import Document
+def main():
+
+    try:
+        # Load the embedding model
+        print("Loading embedding model...")
+       # embeddings = HuggingFaceEmbeddings()
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+if __name__ == "__main__":
+    main()
