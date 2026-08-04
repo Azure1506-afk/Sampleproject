7466c9ad0ecb4ff622841118a6e6ec3a6a5d1d6c
Azure1506-afk <rajasekhar.pappalabooks@gmail.com>
2026-08-04 14:25:35 -0400

    Fix import statements and enable embedding model loading in extractpdf.py; add createVecDB.py file

--- extractpdf.py
+++ extractpdf.py
@@ -3,15 +3,18 @@ import langchain
 import pytesseract
 from PyPDF2 import PdfReader
 #from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+#from langchain.schema import Document
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-from langchain.schema import Document
+from langchain_core.documents import Document
 def main():
 
     try:
         # Load the embedding model
         print("Loading embedding model...")
-       # embeddings = HuggingFaceEmbeddings()
+        embeddings = HuggingFaceEmbeddings()
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
